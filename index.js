df75c8c7a990f14362cea4fbb36ee990f3f304ab
thosd <thos.deaton@gmail.com>
2016-09-16 20:57:09 -0400

    added login and signup pages

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ server.get('/', function(request, response){
   response.sendFile('public/html/index.html', { root: __dirname });
 });
 
+server.get('/signup', function(request, response){
+  response.sendFile('public/html/signup.html', { root: __dirname });
+});
+server.get ('/login', function(request, response){
+  response.sendFile('public/html/login.html', { root: __dirname });
+});
+
 server.listen(8080, function(){
   console.log('Now listening on port...',8080);
 });
